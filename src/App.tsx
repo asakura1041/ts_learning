305b66eeb778c3f482f3960245248ad88e8af04d
asakura1041 <toshiteru.asakura@futurelink.co.jp>
2021-03-24 00:10:25 +0900

    learn intersection types

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const userName = "asakura";
+let userAge: number = 21;
+let bool: boolean = true;
+let ary = [true, "asakura", 40];
+
+interface NAME {
+  first: string;
+  last: string | null;
+}
+
+let name : NAME = { first: "asakura", last: null }
+
+const func1 = (x: number, y:number) => {
+  return x + y;
+};
+
+//Intersection types
+type PROFILE = {
+  age: number;
+  city: string;
+}
+
+type LOGIN = {
+  username: string;
+  password: string;
+}
+
+type USER = PROFILE & LOGIN;
+
+const userA: USER = {
+  age: 21,
+  city: "Chiba",
+  username: "asakura",
+  password: "asakura1041"
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
